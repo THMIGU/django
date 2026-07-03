b925c778b18a2d76f9bb2e02e734597642a225c2
Kai Parsons <thmigu.kp@gmail.com>
2026-07-03 13:26:35 -0700

    feat: basic ping-pong code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+use anyhow::Result;
+use serenity::{
+	Client,
+	all::{Context, EventHandler, GatewayIntents, Message},
+	async_trait,
+};
+
+use crate::config::Config;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+	async fn message(&self, ctx: Context, msg: Message) {
+		if msg.content == "!ping" {
+			if let Err(why) = msg
+				.channel_id
+				.say(&ctx.http, "Pong!")
+				.await
+			{
+				println!("Error sending message: {why:?}")
+			}
+		}
+	}
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	let config: Config = toml::from_str(include_str!("../config.toml"))?;
+
+	let token = config.token;
+	let intents = GatewayIntents::all();
+
+	let mut client = Client::builder(&token, intents)
+		.event_handler(Handler)
+		.await?;
+
+	client.start().await?;
+
+	Ok(())
 }
