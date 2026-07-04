0bceb800694f058254388d3ed122bfc264652d18
Kai Parsons <thmigu.kp@gmail.com>
2026-07-03 17:20:22 -0700

    feat: switched to poise and added slash commands

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,68 @@ mod config;
 
 use std::fs;
 
-use anyhow::Result;
-use serenity::{
-	Client,
-	all::{Context, EventHandler, GatewayIntents, Message},
-	async_trait,
-};
+use anyhow::{Context, Result};
+use poise::serenity_prelude as serenity;
 
 use crate::config::Config;
 
-struct Handler;
+struct Data {}
+type Ctx<'a> = poise::Context<'a, Data, anyhow::Error>;
 
-#[async_trait]
-impl EventHandler for Handler {
-	async fn message(&self, ctx: Context, msg: Message) {
-		if msg.content == "!ping" {
-			if let Err(why) = msg
-				.channel_id
-				.say(&ctx.http, "Pong!")
-				.await
-			{
-				println!("Error sending message: {why:?}")
-			}
-		}
-	}
+#[poise::command(slash_command)]
+async fn ping(ctx: Ctx<'_>) -> Result<()> {
+	let _user = ctx.author();
+	ctx.say("Pong!").await?;
+
+	Ok(())
+}
+
+#[poise::command(slash_command)]
+async fn shutdown(ctx: Ctx<'_>) -> Result<()> {
+	let _user = ctx.author();
+	ctx.say("Shutting down!")
+		.await?;
+
+	let shard_manager = ctx
+		.framework()
+		.shard_manager
+		.clone();
+	let shard_messenger = &ctx.serenity_context().shard;
+
+	shard_messenger.set_status(serenity::OnlineStatus::Offline);
+	shard_manager
+		.shutdown_all()
+		.await;
+
+	Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-	let config_str = fs::read_to_string("config.toml")?;
-	let config: Config = toml::from_str(&config_str)?;
+	let config_str = fs::read_to_string("config.toml").context("Failed to read config.toml")?;
+	let config: Config = toml::from_str(&config_str).context("Failed to parse config.toml")?;
 
 	let token = config.token;
-	let intents = GatewayIntents::all();
+	let intents = serenity::GatewayIntents::all();
 
-	let mut client = Client::builder(&token, intents)
-		.event_handler(Handler)
+	let framework = poise::Framework::builder()
+		.options(poise::FrameworkOptions {
+			commands: vec![ping(), shutdown()],
+			..Default::default()
+		})
+		.setup(|ctx, _ready, framework| {
+			Box::pin(async move {
+				poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+
+				println!("Ready!");
+				Ok(Data {})
+			})
+		})
+		.build();
+
+	let mut client = serenity::ClientBuilder::new(&token, intents)
+		.framework(framework)
 		.await?;
-
 	client.start().await?;
 
 	Ok(())
